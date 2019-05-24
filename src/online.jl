ae342db983274671ba1f8c52d20a351a05657124
adrien-le-franc <adrien.le-franc@enpc.fr>
2019-05-24 19:14:21 +0200

    uncomplete online SDP

--- src/online.jl
+++ src/online.jl
@@ -3,3 +3,11 @@
 # online step for Stochastic Dynamic Programming 
 
 
+# SDP
+
+function compute_control(sdp::SDP, cost::Function, dynamics::Function, 
+	state::Array{Float64,1}, t::Int64, value_functions::ValueFunctions)
+
+	
+
+end
